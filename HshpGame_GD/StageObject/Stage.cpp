baa0600499871a674b94e43b735cd249e78cf574
東 愁汰 <fko2247067@stu.o-hara.ac.jp>
2023-02-08 18:40:24 +0900

    stage first

--- HshpGame_GD/StageObject/Stage.cpp
+++ HshpGame_GD/StageObject/Stage.cpp
@@ -295,7 +295,7 @@ namespace
 Stage::Stage() :
 	m_updateFunc(&Stage::NormalUpdate),
 	m_pPlayer(nullptr),
-	m_stageState(StageState::seventhStage),
+	m_stageState(StageState::firstStage),
 	m_stage(),
 	m_scroll(0),
 	m_scrollAcc(kScrollSpeed),
